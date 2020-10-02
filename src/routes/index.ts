6b8b87ef948292e3ac80d86d451db88be618a465
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-10-02 12:20:09 -0300

    :memo: Updating root route response for goBarber application.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,18 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({
-  title: 'API Boilerplate 🚧',
-  technologies: ['TypeScript', 'Node', 'Express'],
-  version: '1.0.0',
-  author: {
-    name: 'Gabriela Carvalho Camilo',
-    gitHub: 'http://github.com/gabcamilo',
-    linkedIn: 'https://www.linkedin.com/in/gabccamilo/',
-  },
-  licence: 'MIT',
-}));
+routes.get('/', (req, res) =>
+  res.json({
+    title: 'GoBarber 💈',
+    technologies: ['TypeScript', 'Node', 'Express'],
+    version: '1.0.0',
+    author: {
+      name: 'Gabriela Carvalho Camilo',
+      gitHub: 'http://github.com/gabcamilo',
+      linkedIn: 'https://www.linkedin.com/in/gabccamilo/',
+    },
+    licence: 'MIT',
+  }),
+);
 
 export default routes;
