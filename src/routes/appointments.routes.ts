a9bd2064bdc9fd86ca12cae82981b996b2a56c97
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-10-02 16:35:22 -0300

    :construction: Implementing DTO to transfer data between files

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -7,6 +7,11 @@ const appointmentsRouter = Router();
 
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
+
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
   const parsedDate = startOfHour(parseISO(date));
@@ -21,7 +26,10 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
   return res.json(appointment);
 });
 
