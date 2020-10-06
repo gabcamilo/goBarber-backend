1006915c674f5280efde3c40d4b13dc6989d8799
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-10-06 18:49:21 -0300

    :construction: Adding update user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,7 +4,9 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -29,8 +31,18 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (req, res) => {
-    console.log(req.file);
-    return res.send();
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
   },
 );
 
