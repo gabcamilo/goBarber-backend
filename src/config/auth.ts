6856b4b2452ff75a9b2309ad275deffbdc649d3a
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-10-06 15:20:28 -0300

    :construction: Ensure athentication on appointment routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'temporarykey',
+    expiresIn: '1d',
+  },
+};
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { Response, Request, NextFunction, request } from 'express';
+import { decode, verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+export default function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  // validate JWT
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new Error('Invalid JWT token');
+  }
+}
